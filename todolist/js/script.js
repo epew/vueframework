35de5028181e2efa8381b60277566c1ad70d6535
Florina Madalina TIVLEA <ftivlea@bbu.bitdefender.biz>
2017-07-05 16:59:06 +0300

    added working example

--- todolist/js/script.js
+++ todolist/js/script.js
@@ -1,35 +1,37 @@
-console.log("hei there <3 ");
 new Vue ({
-  el : '#todolist',
-  data : {
-    title: 'to do list',
-    tasks: [
-      {
-        name : 'drink coffee',
-        del : '-'
-      },
-      {
-        name : 'read a book',
-        del : '-'
-      },
-      {
-        name : 'eat healthy',
-        del : '-'
-      }
-    ]
-  },
-  methods : {
-    newItem : function(){
-      if(!this.task.name){
-        return
-      }
-      this.tasks.push({
-        name : this.tasks.name,
-        del: '-'
-      })
-    },
-    delItem: function(task){
-      this.tasks.splice(this.tasks.indexOf(task), 1)
-    }
-  }
+
+	el: '#toDoList',
+	data: {
+		title: 'to do list',
+		tasks: [
+			{
+				name: 'drink a cup of coffee',
+				del: '-'
+			},
+			{
+				name: 'read a book',
+				del: '-'
+			},
+			{
+				name: 'eat pizza',
+				del: '-'
+			}
+		]
+	},
+
+	methods: {
+		newItem: function() {
+			if (!this.tasks.name) {
+				return
+			}
+			this.tasks.push ( {
+				name: this.tasks.name,
+				del: '-'
+			})
+		},
+		delItem: function (task) {
+			this.tasks.splice(this.tasks.indexOf(task), 1)
+		}
+	}
+
 })
