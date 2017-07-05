24a10dd574b06c930e4fb8afd37ab442c664bf05
Florina Madalina TIVLEA <ftivlea@bbu.bitdefender.biz>
2017-07-05 16:22:06 +0300

    js

--- todolist/js/script.js
+++ todolist/js/script.js
@@ -1,1 +1,35 @@
 console.log("hei there <3 ");
+new Vue ({
+  el : '#todolist',
+  data : {
+    title: 'to do list',
+    tasks: [
+      {
+        name : 'drink coffee',
+        del : '-'
+      },
+      {
+        name : 'read a book',
+        del : '-'
+      },
+      {
+        name : 'eat healthy',
+        del : '-'
+      }
+    ]
+  },
+  methods : {
+    newItem : function(){
+      if(!this.task.name){
+        return
+      }
+      this.tasks.push({
+        name : this.tasks.name,
+        del: '-'
+      })
+    },
+    delItem: function(task){
+      this.tasks.splice(this.tasks.indexOf(task), 1)
+    }
+  }
+})
